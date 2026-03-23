37da63ef76c7ec2e937aa22b9969bbb93f1c530a
Retr0IsNotAvailable <josephmagodks15846@gmail.com>
2026-03-23 02:42:11 +0000

    implement subject dynamic adding functionality to the list

--- js/index.js
+++ js/index.js
@@ -8,9 +8,10 @@ const timeInput = document.querySelector(".timeInput");
 const subjectInput = document.querySelector(".subjectInput");
 const addButton = document.querySelector(".addButton");
 
-// span time, span subject DOM's
+// span time, span subject and sujects' list DOM's
 const spanTime = document.querySelector(".spanTime");
 const spanSubject = document.querySelector(".spanSubject");
+const subjectsList = document.querySelector(".subjects");
 
 // get time spent on subject
 function getTime() {
@@ -22,7 +23,25 @@ function getSubject() {
   return subjectInput.value;
 }
 
+// create a subject and give a time and append it
+function addSubject(time, subject) {
+  const li = document.createElement("li");
+  const spanTime = document.createElement("span");
+  const spanSubject = document.createElement("span");
+
+  spanTime.innerHTML = time + " ";
+  spanSubject.innerHTML = subject;
+
+  li.appendChild(spanTime);
+  li.appendChild(spanSubject);
+
+  return li;
+}
+
 addButton.addEventListener("click", () => {
-  spanTime.innerHTML = getTime();
-  spanSubject.innerHTML = getSubject();
+  const time =  getTime();
+  const subject = getSubject();
+
+  const newList = addSubject(time, subject);
+  subjectsList.appendChild(newList);
 });
